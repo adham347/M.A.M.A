94521409c65045b993beba509c5fab89e7077749
adham347 <adham347@gmail.com>
2023-05-25 01:49:30 +0300

    done

--- ordersController.js
+++ ordersController.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
-import { doc, getDocs, setDoc, getFirestore, collection, Timestamp, addDoc, orderBy, query } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js"
+import { doc, deleteDoc, getDocs, setDoc, getFirestore, collection, Timestamp, addDoc, orderBy, query } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,9 +26,9 @@ var docRef = collection(fs, "orders");
 const q = query(docRef, orderBy("timestamp"));
 const snapshot = await getDocs(q);
 
-snapshot.docs.forEach(doc => {
-    console.log(doc.id);
-    console.log(doc.data());
+snapshot.docs.forEach(Doc => {
+    console.log(Doc.id);
+    console.log(Doc.data());
 
     let container = document.getElementById("ordersContainer");
 
@@ -38,12 +38,12 @@ snapshot.docs.forEach(doc => {
     orderItem.appendChild(document.createElement("hr"));
 
     let tableNo = document.createElement("h3");
-    tableNo.innerHTML = "Table " + doc.data().table;
+    tableNo.innerHTML = "Table " + Doc.data().table;
     orderItem.appendChild(tableNo);
     orderItem.appendChild(document.createElement("br"));
 
 
-    let orderDetails = doc.data().order;
+    let orderDetails = Doc.data().order;
     orderDetails.forEach(element => {
         let item = document.createElement("p");
         item.innerHTML = element;
@@ -52,16 +52,27 @@ snapshot.docs.forEach(doc => {
 
     let orderDate = document.createElement("p");
     let bold = document.createElement("b");
-    bold.innerHTML = "Order Received At: " + doc.data().receivedAt;
+    bold.innerHTML = "Order Received At: " + Doc.data().receivedAt;
     orderDate.appendChild(bold);
     orderItem.appendChild(orderDate);
 
     let button = document.createElement("button");
     button.setAttribute("class", "orderbtn");
-    button.setAttribute("id", "" + doc.id);
+    button.setAttribute("id", "" + Doc.id);
     let buttontxt = document.createElement("b");
     buttontxt.innerHTML = "order done";
     button.appendChild(buttontxt);
+    button.addEventListener("click", () => {
+        const ref = doc(fs, "orders", button.id);
+        deleteDoc(ref)
+            .then(() => {
+                alert("Order Removed");
+                button.parentElement.remove();
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    });
     orderItem.appendChild(button);
 
     orderItem.appendChild(document.createElement("hr"));
